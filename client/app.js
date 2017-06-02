54318ec324e7472b991409928402b1dbea55bae0
Maarten <maartenwachtersmast@gmail.com>
2017-06-02 10:40:20 +0200

    finished

--- client/app.js
+++ client/app.js
@@ -23,9 +23,39 @@ labTutorApp.controller('StartCtrl', [ '$scope', '$http', function ($scope, $http
       $scope.name = err;
     });
   }
-
   $scope.login = function() {
     window.location.href = "https://www.facebook.com/dialog/oauth?client_id=1772117579765567&response_type=token&redirect_uri=http://localhost:3000/"
   };
 
 }]);
+labTutorApp.controller('MainController', ['$scope', '$http', function($scope, $http){
+  
+}
+/*
+labTutorApp.controller('payPalController', ['$scope', '$http', function($scope, $http){
+
+  $scope.pay   = function(req, response, next) {
+    $scope.access_token = "A21AAGBlCKTnPGmOaxfWulG35HSsifDAtgOTi4_eXvbW0-yz_m0WZyk8dM-dM-AuWI4RqOL1qe5-nkQjAXERoR7iiqUOU0I3Q"
+
+    if (params.access_token) {
+      $http({
+        method: 'POST',
+        url: 'https://api.sandbox.paypal.com/v1/payments/payment?&access_token=' + params.access_token,
+        headers: [
+          'Content-Type': 'application/json'
+        ]
+        intent: 'sale',
+        sale: '',
+        authorize: '',
+        order: '',
+        redirect_uri: 'http://localhost:3000'
+      }).then(function (response) {
+        console.log(response);
+      }, function (err) {
+
+      });
+    }
+  };
+}*/
+
+]);
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 
 //Module requirements
 var express = require('express');
-var MongoClient = require('mongodb').MongoClient;
 var bodyparser = require('body-parser');
+
 
 var app = express();
 app.use(express.static('client'));
 app.use(bodyparser.json());
+
+//your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+
+
+// Token is created using Stripe.js or Checkout!
+// Get the payment token submitted by the form:
+
 
 	//Break database connection
 	process.on('SIGINT', function(){
@@ -22,4 +30,23 @@ app.use(bodyparser.json());
 
 	// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
 
+app.post('/pay', function(request,res){
+	// Set your secret key: remember to change this to your live secret key in production
+// See your keys here: https://dashboard.stripe.com/account/apikeys
+var stripe = require("stripe")("sk_test_q3XTqeDRIs2L3ACIr9WpFGmd");
+
+// Token is created using Stripe.js or Checkout!
+// Get the payment token submitted by the form:
+var token = request.body.stripeToken; // Using Express
+
+// Charge the user's card:
+var charge = stripe.charges.create({
+  amount: 1000,
+  currency: "eur",
+  description: "Example charge",
+  source: token,
+}, function(err, charge) {
+  // asynchronously called
+});
+})
 app.listen(3000);
